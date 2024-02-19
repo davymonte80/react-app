cb7bdbab42ba465476889fa42c7bbb5371c984a7
davymonte80 <davymonte80@gmail.com>
2024-02-20 01:36:10 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import Message from './Message';
-function App ()
-{
-  return <div><Message></Message></div>
+import { Fragment } from "react";
+import ListGroup from "./components/ListGroup";
+function App() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">An item</li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
+    </ul>
+  );
 }
-export default App; 
+export default App;
