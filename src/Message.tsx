770d0656b745d01ac027446e611b75af2425c9dc
davymonte80 <davymonte80@gmail.com>
2024-02-20 00:53:40 +0300

    update

--- src/Message.tsx
+++ src/Message.tsx
@@ -3,7 +3,10 @@ import { ReturnDocument } from "mongodb";
 //PascalCasing
 function Message(){
     //Jsx:Javascript XML
-    return <h1>Hello World</h1>;
+    const name ='Monte';
+    if (name)
+     return <h1>Hello {name}</h1>;
+     return <h1>Hello World</h1>
 
 }
 export default Message;
