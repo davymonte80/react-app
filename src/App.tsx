3ce237ad60519d5b09b1d08ae902b256c73c5355
davymonte80 <davymonte80@gmail.com>
2024-02-20 18:53:17 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Fragment } from "react";
 import ListGroup from "./components/ListGroup";
 function App() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <Fragment>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </Fragment>
   );
 }
 export default App;
