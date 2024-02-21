6e6b0126651bc3a7655badb61e755079baeb73ea
davymonte80 <davymonte80@gmail.com>
2024-02-21 22:58:19 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import ListGroup from "./components/ListGroup";
-const items =[ 
-  'Nairobi',
-  'Nakuru',
-  'Nanyuki',
-  'Kisii',
-  'Kisumu',
-]
+
+const items = ["Nairobi", "Nakuru", "Nanyuki", "Kisii", "Kisumu"];
 
 // Declare the function named myFunction
 function myFunction() {
   alert("Hello World!");
 }
 
-{items.map(item =><li> {item}</li>)}
+// Define the functional component App
+const App = () => {
   return (
     <Fragment>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
     </Fragment>
   );
-}
+};
+
 export default App;
